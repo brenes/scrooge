8b7a48a3e1c65234a537d6a4d908279056127d6a
David J. Brenes <davidjbrenes@gmail.com>
2016-10-26 00:04:13 +0200

    Securing the sentences scope by hashing the robot api key
    ¯\_(ツ)_/¯

--- config/initializers/fantastic_robot.rb
+++ config/initializers/fantastic_robot.rb
@@ -6,7 +6,7 @@ FantasticRobot.configure do |config|
   config.delivery_method = :webhook
 
   # Callback URL of the webhook
-  config.callback_url = "#{Rails.application.secrets.telegram_host}/sentences"
+  config.callback_url = "#{Rails.application.secrets.telegram_host}/#{Digest::SHA256.hexdigest(Rails.application.secrets.telegram_api_key)}/sentences"
 end
 
 FantasticRobot.initialize! if(defined?(::Thin) || defined?(::Unicorn) || defined?(::Passenger) || defined?(::Puma))
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require 'digest'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sentences, only: :create
+  scope Digest::SHA256.hexdigest(Rails.application.secrets.telegram_api_key) do
+    resources :sentences, only: :create
+  end
+
 end
